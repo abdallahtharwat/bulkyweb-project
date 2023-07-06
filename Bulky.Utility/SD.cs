c04b77c9982cb74e7d1b24e6dc2360a5bc013912
Abdelrahman Tharwat <atharwat@spark-sys.com>
2023-07-06 21:50:17 +0300

    add neww

--- Bulky.Utility/SD.cs
+++ Bulky.Utility/SD.cs
@@ -12,6 +12,7 @@ namespace BulkyBook.Utility
         public const string Role_Company = "Company";
         public const string Role_Admin = "Admin";
         public const string Role_Employee = "Employee";
+        
       
 
 
